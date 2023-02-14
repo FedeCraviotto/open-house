ed5b29a6ece18b21fde6eaa584cf7ce0b59d1afa
FedeCraviotto <fcraviotto.dev@gmail.com>
2023-02-14 11:17:13 -0300

    adding toolkit

--- client/src/store.js
+++ client/src/store.js
@@ -1,14 +1,14 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
 
 const initialState = {};
 
 
-const store = createStore(
-    // Busca el index en la carpeta indicada, en vez de setearlos en Slices y  combinarlos aca.
-    rootReducer,
+const store = configureStore({
     initialState,
-
+    // Busca el index en la carpeta indicada, en vez de setearlos en Slices y  combinarlos aca.
+    reducer: rootReducer,
+}
 )
 
 export default store;
